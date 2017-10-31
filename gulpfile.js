e4b1e29198e553348889fe170566365fc31fd49c
LemoineSimon <lemoine.simon.perso@gmail.com>
2017-10-31 23:24:36 +0100

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const babel = require('gulp-babel');
---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+'use strict'
+
+(function() {
+    const game = new Game();
+    game.addHive();
+
+    let farm = new Vue({
+        el: "#farm",
+        data: {
+            'hives': game.hives
+        },
+        methods: {
+
+            addPrincess: function(index) {
+                this.hives[index].addBee("princess");
+            },
+            addDrone: function(index) {
+                this.hives[index].addBee("drone");
+            },
+            start: function(index) {
+                this.hives[index].start();
+            },
+
+            collectPrincess: function(hiveIndex) {
+                game.collectPrincess(hiveIndex);
+            },
+            collectDrone: function(hiveIndex, droneIndex) {
+                game.collectDrone(hiveIndex, droneIndex);
+            }
+        },
+        computed: {
+            currentState: function() {
+                return this.hives.map(function(hive) {
+                    return hive.states[hive.currentState].type;
+                });
+            }
+        }
+    });
+
+    let statusBar = new Vue({
+        el: "#statusBar",
+        data: {
+            'money': game.money,
+            'bees': game.bees,
+            'hives': game.hives.length
+        }
+    })
+})();
