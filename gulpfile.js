8494fc447ba91f943808ca7a0840efd424e3fe60
LemoineSimon <lemoine.simon.perso@gmail.com>
2017-11-01 00:22:19 +0100

    SCSS is the first step

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,48 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 const babel = require('gulp-babel');
+
+const path = {
+    css: 'dist/css/',
+    scss: {
+        watch: 'src/scss/**/*.scss',
+        src: 'src/scss/page/*.scss'
+    },
+    js: {
+        src: 'src/js/*.js',
+        build: 'dist/js'
+    }
+}
+
+const autoprefixerOpts = {
+    browsers: [
+        'last 2 version',
+        'ie >= 11',
+        'opera >= 36',
+        'android >= 4'
+    ],
+    cascade: false
+};
+
+const cleanCssConfig = {
+    debug: true,
+    level: {
+        2: {
+            all: false,
+            removeDuplicatedRules: true
+        }
+    }
+};
+
+gulp.task('scss', function() {
+    gulp.src(path.scss.src)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOpts))
+        .pipe(cleanCSS(cleanCssConfig))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest(path.css));
+});
